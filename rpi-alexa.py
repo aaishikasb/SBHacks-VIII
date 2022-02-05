b491ec03ba6e2c8d8c721d532eb76170d23ea343
aaishikasb <mail.aaishika@gmail.com>
2022-02-05 15:31:57 +0530

    🚩 Add Raspberry Pi Code

---
+++ rpi-alexa.py
@@ -0,0 +1,56 @@
+import logging
+import os
+import serial
+
+from flask import Flask
+from flask_ask import Ask, request, session, question, statement
+
+app = Flask(__name__)
+ask = Ask(app, "/")
+logging.getLogger('flask_ask').setLevel(logging.DEBUG)
+
+@ask.launch
+def launch():
+    speech_text = 'Welcome to Sand Pie.'
+    return question(speech_text).reprompt(speech_text).simple_card(speech_text)
+
+
+@ask.intent('BeachIntent')
+def Beach_Intent():
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.flush()
+
+    while True:
+        if ser.in_waiting > 0:
+            ser_data = ser.readline().decode('utf-8').rstrip()
+
+    if ser_data < 350:
+        return statement('Be careful, the weather and waves at Campus Point look concerning.')
+    elif ser_data > 350:
+        return statement('Weather and waves at Campus Point are looking good!')
+    else:
+        return statement('Sorry, data could not be processed.')
+
+@ask.intent('StatusIntent')
+def Status_Intent():
+    speech_text = 'The hardware is healthy and fully functional.'
+    return question(speech_text).reprompt(speech_text).simple_card(speech_text)
+
+
+@ask.intent('AMAZON.HelpIntent')
+def help():
+    speech_text = 'You can ask me about the weather at Campus Point or the status of the apparatus!'
+    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+
+
+@ask.session_ended
+def session_ended():
+    return "{}", 200
+
+
+if __name__ == '__main__':
+    if 'ASK_VERIFY_REQUESTS' in os.environ:
+        verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
+        if verify == 'false':
+            app.config['ASK_VERIFY_REQUESTS'] = False
+    app.run(debug=True)
