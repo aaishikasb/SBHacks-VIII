c71551f47b7444a173a1b1602900610115ba1e22
aaishikasb <mail.aaishika@gmail.com>
2022-02-05 20:57:35 +0530

    📱 Add Twilio Integration

--- rpi-alexa.py
+++ rpi-alexa.py
@@ -1,6 +1,8 @@
 import logging
 import os
+from unittest import FunctionTestCase
 import serial
+from twilio.rest import Client
 
 from flask import Flask
 from flask_ask import Ask, request, session, question, statement
@@ -8,6 +10,25 @@ from flask_ask import Ask, request, session, question, statement
 app = Flask(__name__)
 ask = Ask(app, "/")
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
+
+# Twilio Credentials
+account_sid = 'ACdaeeb95cef452cff95c81234757f1b95'
+auth_token = '5ce4496a55b7ca3770ec4a731aca88ce'
+
+client = Client(account_sid, auth_token)
+
+# Twilio Functions
+def bad_message():
+        client.messages.create(
+            to='whatsapp:+917827794110',
+            from_='whatsapp:+17752568757',
+            body="Hey, looks like it's a bad idea to go to Campus Point today. In case you decide to go, take proper precautions!")
+
+def good_message():
+        client.messages.create(
+            to='+917827794110',
+            from_='whatsapp:+17752568757',
+            body="The weather and waves at Campus Point are looking good! If you're planning on going, you'll have a great time!")
 
 @ask.launch
 def launch():
@@ -21,9 +42,13 @@ def Beach_Intent():
     ser_data = ser.readline().decode('utf-8').rstrip()
 
     if int(ser_data) < 350:
+        bad_message()
         return statement('Be careful, the weather and waves at Campus Point look concerning.')
+
     elif int(ser_data) > 350:
+        good_message()
         return statement('Weather and waves at Campus Point are looking good!')
+        
     else:
         return statement('Sorry, data could not be processed.')
 
@@ -52,7 +77,6 @@ def cancel():
 @ask.session_ended
 def session_ended():
     return "{}", 200
-
 
 if __name__ == '__main__':
     if 'ASK_VERIFY_REQUESTS' in os.environ:
